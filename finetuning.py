f9f13824bb3995e315612f73395bf81f3ba8b5bb
Emma van Breukelen <559643eb@eur.nl>
2025-07-01 21:14:17 +0200

    Update finetuning.py

--- finetuning.py
+++ finetuning.py
@@ -1,1 +1,23 @@
+import json
+import torch
+from unsloth import FastLanguageModel
+from sklearn.metrics import accuracy_score, f1_score, classification_report
+from tqdm import tqdm
+import huggingface_hub
+from huggingface_hub import login
+from transformers import TrainingArguments, DataCollatorForLanguageModeling, AutoModel, AutoTokenizer
+from trl import SFTTrainer
+from peft import LoraConfig
+import wandb
+import uuid
+from datasets import Dataset
+from typing import Dict, List, Any
+import os
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+
+
+
 
