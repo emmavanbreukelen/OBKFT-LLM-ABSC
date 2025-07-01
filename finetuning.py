8404f419b7875e550b97c8d02268b3cb7be1dfac
Emma van Breukelen <559643eb@eur.nl>
2025-07-01 21:25:08 +0200

    Update finetuning.py

--- finetuning.py
+++ finetuning.py
@@ -16,24 +16,6 @@ import os
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-import json
-import torch
-from unsloth import FastLanguageModel
-from sklearn.metrics import accuracy_score, f1_score, classification_report
-from tqdm import tqdm
-import huggingface_hub
-from huggingface_hub import login
-from transformers import TrainingArguments, DataCollatorForLanguageModeling, AutoModel, AutoTokenizer
-from trl import SFTTrainer
-from peft import LoraConfig
-import wandb
-import uuid
-from datasets import Dataset
-from typing import Dict, List, Any
-import os
-import numpy as np
-from sklearn.metrics.pairwise import cosine_similarity
-
 # Log in to HuggingFace and Wandb with you pass keys
 key_hf = os.getenv("HUGGINGFACE_KEY")
 try:
